1601a8ea44ea00942086a1b08f58e4e4fb6a6891
Tanngar <31502612+Tanngar@users.noreply.github.com>
2017-09-29 11:11:57 +0200

    The new beginning
    Used photo as header and footer

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="./css/style.css">
+  <link rel="stylesheet" href="./css/reset.css">
+</head>
+<body>
+<img src="./img/header.png">
+</body>
+<img src="./img/footer.png">
+</html>
