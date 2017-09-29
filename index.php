46a12ca8c88ff8f860f3c998143aab89d64e36cd
GeorgiOvalov <31400265+GeorgiOvalov@users.noreply.github.com>
2017-09-29 15:46:38 +0200

    V1

--- index.php
+++ index.php
@@ -1,22 +1,60 @@
 <!DOCTYPE html>
 <html>
-<head>
-  <title></title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="reset.css">
-</head>
-<body>
-	<img src="./img/header.png">
-  <aside id="wrapper">
-<h3>LATEST PURCHASES</h3>
-<div class="product-box">
-</div>
-</aside>
-<main id="wrapper2">
+    <head>
+        <title></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="reset.css">
+    </head>
+    <body>
+        <img src="./img/header.png">
+        <aside id="wrapper">
+            <div class="aside-menu">
+            </div>        
+            <ul>    
+                <h1>My Grundfos</h1>
 
-</main>
-</body>
-<img src="./img/footer.png">
+                <ul class="nav left-nav">
+                    <li>
+                        <a href="#" class="links">Dashboard</a>
+                    </li> 
+                </ul>
+                <h1>Tools</h1>
+                <div class="nav">
+                <ul class="nav left-nav">    
+                    <li>
+                        <a href="#" class="links">Spare parts finder</a>
+                    </li>   
+                    <li>
+                        <a href="#" class="links">Pump replacement</a>
+                    </li>       
+                    <li>
+                        <a href="#" class="links">Quotations</a>
+                    </li>        
+                    <li>
+                        <a href="#" class="links">Order status</a>
+                    </li>         
+                    <li>
+                        <a href="#" class="links">List price finder</a>
+                    </li>        
+                    <li>
+                        <a href="#" class="links">Availability</a>
+                    </li>   
+                    <li>
+                        <a href="#" class="links">Marketing materials</a>
+                    </li> 
+                </ul>
+                </div>
+            </ul>
+
+            <h3>LATEST PURCHASES</h3>
+            <div class="product-box">
+            </div>
+        </aside>
+        <main id="wrapper2">
+
+        </main>
+    </body>
+    <img src="./img/footer.png">
 </html>
