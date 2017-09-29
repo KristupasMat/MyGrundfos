6a697fc6b46f3f5b6a654c73ac8b247a8b650f6c
Tanngar <31502612+Tanngar@users.noreply.github.com>
2017-09-29 14:44:41 +0200

    Search bars

--- index.php
+++ index.php
@@ -4,18 +4,47 @@
   <title></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<img src="./img/header.png">
-  <aside id="wrapper">
-<h3>LATEST PURCHASES</h3>
-<div class="product-box">
+<img src="./img/header.png">
+<div id="container">
+	<aside id="wrapper">
+	TEST
+	</aside>
+	<main id="wrapper2">
+	<h1 id="main-heading">MARKETING MATERIALS</h1>
+	<!-- Search form for marketing materials -->
+	<div class="search-form">
+		<label><h2>Search for marketing materials:</h2></label>
+		<form>
+			 <input type="text" class="search-input" name="product">
+			 <input class="search-btn" type="submit" id="find-materials" value="SEARCH">
+		</form>
+	</div>
+	<!-- Search form for adding products to favourites -->
+	<div class="search-form">
+		<label><h2>Find a product you would like to add to your favourites:</h2></label>
+		<form>
+			 <input type="text" class="search-input" name="product">
+			 <input class="search-btn" type="submit" id="add-product" value="ADD PRODUCT TO FAVOURITES">
+		</form>
+	</div>
 </div>
-</aside>
-<main id="wrapper2">
-
+<!-- Section that will hold the notificaition message -->
+<a href="#">
+	<div id="notification">
+			<h2>New feature available!</h2>
+			<p>Use the newly updated marketing materials module to promote your products and increse your sales.</p>
+	</div>
+</a>
+<script type="text/javascript">
+	//Bring up the notification on any keypress
+	var notification = document.getElementById("notification");
+	window.addEventListener("keydown", function(e) {
+		notification.style.display ="block"});
+</script>
 </main>
 </body>
 <img src="./img/footer.png">
