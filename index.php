c86d5bd6240c98cb6b90fd91fd6f8d6333c4f6fa
Tanngar <31502612+Tanngar@users.noreply.github.com>
2017-09-29 11:25:53 +0200

    Changes

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
   <title></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="css.css">
+  <link rel="stylesheet" href="reset.css">
 </head>
 <body>
-<img src="./img/header.png">
+	<img src="./img/header.png">
 </body>
 <img src="./img/footer.png">
 </html>
