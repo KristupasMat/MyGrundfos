966d5c1a9aa5166498079172e6ba1d02451eef8d
Denisdanezu <denis.muntianu@gmail.com>
2017-09-29 14:59:14 +0200

    Latest purchases

--- index.php
+++ index.php
@@ -4,17 +4,59 @@
   <title></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="reset.css">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
 </head>
 <body>
 	<img src="./img/header.png">
   <aside id="wrapper">
 <h3>LATEST PURCHASES</h3>
+
+<section>
 <div class="product-box">
+<div class="updated-tag">
+  UPDATED
 </div>
+<i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
+</div>
+<p class="pump-name">ALPHA 1</p>
+<button type="button" class="download-button">DOWNLOAD</button>
+</section>
+
+<section class="section-latest">
+<div class="product-box">
+<div class="updated-tag">
+  UPDATED
+</div>
+<i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
+</div>
+<p class="pump-name">ALPHA 2</p>
+<button type="button" class="download-button">DOWNLOAD</button>
+</section>
+
+<section class="section-latest">
+<div class="product-box">
+
+<i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
+</div>
+<p class="pump-name">ALPHA 3</p>
+<button type="button" class="download-button">DOWNLOAD</button>
+</section>
 </aside>
 <main id="wrapper2">
+
+    <section class="favorite">
+    <div class="product-box">
+    <div class="updated-tag">
+      UPDATED
+    </div>
+    <i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
+    </div>
+    <p class="pump-name">ALPHA 1</p>
+    <button type="button" class="download-button">DOWNLOAD</button>
+    </section>
+  </div>
 
 </main>
 </body>
