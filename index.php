3bae44d24dce0aeafc40f773e25aac23f4e4c48c
Tanngar <31502612+Tanngar@users.noreply.github.com>
2017-10-03 13:04:53 +0200

    Search bar and add product functionality

--- index.php
+++ index.php
@@ -1,102 +1,95 @@
 <!DOCTYPE html>
 <html>
-
-
-
 <head>
   <title>My Grundfos</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="reset.css">
   <link rel="stylesheet" href="style.css">
-  <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
-
 </head>
 <body>
 <img src="./img/header.png" class="header">
 <div id="container">
   <aside id="wrapper">
-      
-      <section class="component component component-pagetitle">
-      <h1><b>MyGrundfos</b></h1>
-      </section>
-      <ul class="nav left-nav">
-                    <li class="links">
-                        Dashboard
-                    </li> 
-                </ul>
-                <h1><b>Tools</b></h1>
-                <div class="nav">
-                <ul class="nav left-nav">    
-                    <li class="links1">
-                        Spare parts finder
-                    </li>   
-                    <li class="links1">
-                        Pump replacement
-                    </li>       
-                    <li class="links1">
-                        Quotations
-                    </li>        
-                    <li class="links1">
-                        Order status
-                    </li>         
-                    <li class="links1">
-                        List price finder
-                    </li>        
-                    <li class="links1">
-                        Availability
-                    </li>   
-                    <li class="links1">
-                        Marketing materials
-                    </li> 
-                </ul>
-    </div>
-      
+        <section class="component component-pagetitle">
+      		<h1><b>MyGrundfos</b></h1>
+        </section>
+      	<ul class="nav left-nav">
+            <li class="links">
+                Dashboard
+            </li> 
+        </ul>
+        <h1><b>Tools</b></h1>
+        <div class="nav">
+		    <ul class="nav left-nav">    
+		        <li class="links1">
+		            Spare parts finder
+		        </li>   
+		        <li class="links1">
+		            Pump replacement
+		        </li>       
+		        <li class="links1">
+		            Quotations
+		        </li>        
+		        <li class="links1">
+		            Order status
+		        </li>         
+		        <li class="links1">
+		            List price finder
+		        </li>        
+		        <li class="links1">
+		            Availability
+		        </li>   
+		        <li class="links1">
+		            Marketing materials
+		        </li> 
+		    </ul>
+		</div>
 <h3>LATEST PURCHASES</h3>
 <div class="latest-purchases">
 	<div class="product" id="latest-product1">
 		<div class="product-box">
-			<img src="img/ALPHA1.png" width="auto" height="160px">
+			<img src="img/Alpha1L.jpg" width="auto" height="160px">
 			<div class="updated-tag">
 				UPDATED
 			</div>
-			<button class="add-favorite-btn" id="add-product1">
+			<button class="add-favorite-btn functional-btn" id="add-product1">
 				<img src="./img/favorite-icon.svg">
 			</button>
-			<button class="remove-btn" id="delete-latest-product1">
-					
+			<button class="remove-btn functional-btn" id="delete-latest-product1">
+				<img src="./img/delete-icon.svg">	
 			</button>
 		</div>
-		<p class="pump-name">ALPHA 1</p>
-		<button type="button" class="download btn" onclick="openPopup()">DOWNLOAD</button>
+		<p class="pump-name">ALPHA1 L</p>
+		<button type="button" class="download btn secondary-btn" onclick="openPopup()">DOWNLOAD</button>
 	</div>
 	<div class="product" id="latest-product2">
 		<div class="product-box">
-			<img src="img/alpha2.jpg" width="auto" height="160px">
+			<img src="img/CMBE.jpg" width="auto" height="160px">
 			<div class="updated-tag">
 				UPDATED
 			</div>
-			<button class="add-favorite-btn" id="add-product2">
+			<button class="add-favorite-btn functional-btn" id="add-product2">
 				<img src="./img/favorite-icon.svg">
 			</button>
-			<button class="remove-btn" id="delete-latest-product2">
-					
+			<button class="remove-btn functional-btn" id="delete-latest-product2">
+				<img src="./img/delete-icon.svg">	
 			</button>
 		</div>
-		<p class="pump-name">ALPHA 2</p>
-		<button type="button" class="download btn" onclick="openPopup()">DOWNLOAD</button>
+		<p class="pump-name">CMBE</p>
+		<button type="button" class="download btn secondary-btn" onclick="openPopup()">DOWNLOAD</button>
 	</div>
 	<div class="product" id="latest-product3">
 		<div class="product-box">
-			<img src="img/alpha3.jpg" width="auto" height="160px">
-			<button class="add-favorite-btn" id="add-product3">
+			<img src="img/CMB-PM1.jpg" width="auto" height="160px">
+			<button class="add-favorite-btn functional-btn" id="add-product3">
 				<img src="./img/favorite-icon.svg">	
 			</button>
-			<button class="remove-btn" id="delete-latest-product3">
-					
+			<button class="remove-btn functional-btn" id="delete-latest-product3">
+				<img src="./img/delete-icon.svg">
 			</button>
-			<p class="pump-name">ALPHA 3</p>
-			<button type="button" class="download btn" onclick="openPopup()">DOWNLOAD</button>
+			<p class="pump-name">CMB-PM1</p>
+			<button type="button" class="download btn secondary-btn" onclick="openPopup()">DOWNLOAD</button>
 		</div>
 	</div>
 </div>
@@ -107,8 +100,8 @@
 	<div class="search-form">
 		<label><h2>Search for marketing materials:</h2></label>
 		<form>
-			 <input type="text" class="search-input" name="product">
-			 <input class="btn" type="submit" id="find-materials" value="SEARCH">
+			 <input type="text" class="search-input" id="search-product" name="product">
+			 <input class="btn" type="button" id="find-materials" value="SEARCH" onclick="redirectSearch()">
 		</form>
 	</div>
 <div class="favorite-row">
@@ -118,11 +111,11 @@
 			<div class="updated-tag">
 				UPDATED
 			</div>
-			<button class="remove-btn" id="delete-favorite-product1">
+			<button class="remove-btn functional-btn" id="delete-favorite-product1">
 				<img src="./img/delete-icon.svg">	
 			</button>
 			<p class="pump-name">ALPHA 1</p>
-			<button type="button" class="download btn fav" onclick="openPopup()">DOWNLOAD</button>
+			<button type="button" class="download btn fav secondary-btn" onclick="openPopup()">DOWNLOAD</button>
 		</div>
 	</div>
 	<div class="favorite" id="product2">
@@ -131,21 +124,31 @@
 			<div class="updated-tag">
 				UPDATED
 			</div>
-			<button class="remove-btn" id="delete-favorite-product2">
+			<button class="remove-btn functional-btn" id="delete-favorite-product2">
 				<img src="./img/delete-icon.svg">	
 			</button>
 			<p class="pump-name">ALPHA 2</p>
-			<button type="button" class="download btn fav" onclick="openPopup()">DOWNLOAD</button>
+			<button type="button" class="download btn fav secondary-btn" onclick="openPopup()">DOWNLOAD</button>
 		</div>
 	</div>
   	<div class="favorite" id="product3">
 		<div class="favorite-box">
 			<img src="img/alpha3.jpg" width="auto" height="160px">
-			<button class="remove-btn" id="delete-favorite-product3">
+			<button class="remove-btn functional-btn" id="delete-favorite-product3">
 				<img src="./img/delete-icon.svg">	
 			</button>
 			<p class="pump-name">ALPHA 3</p>
-			<button type="button" class="download btn fav" onclick="openPopup()">DOWNLOAD</button>
+			<button type="button" class="download btn fav secondary-btn" onclick="openPopup()">DOWNLOAD</button>
+		</div>
+	</div>
+	<div class="favorite" id="product4">
+		<div class="favorite-box">
+			<img src="img/Magna1.png" width="auto" height="160px">
+			<button class="remove-btn functional-btn" id="delete-favorite-product3">
+				<img src="./img/delete-icon.svg">	
+			</button>
+			<p class="pump-name">MAGNA1</p>
+			<button type="button" class="download btn fav secondary-btn" onclick="openPopup()">DOWNLOAD</button>
 		</div>
 	</div>
 </div>
@@ -154,8 +157,8 @@
 <div class="search-form">
 	<label><h2>Find a product you would like to add to your favourites:</h2></label>
 	<form>
-		 <input type="text" class="search-input" name="product">
-		 <input class="btn" type="submit" id="add-product" value="ADD PRODUCT TO FAVOURITES">
+		 <input type="text" class="search-input" id="add-product" name="product">
+		 <input class="btn functional-btn" type="button" value="ADD PRODUCT TO FAVOURITES" onclick="addProduct()">
 	</form>
     </div>
 </div>
@@ -163,113 +166,15 @@
 <!-- Section that will hold the notificaition message -->
 <a href="#">
 	<div id="notification">
-			<h2>New feature available!</h2>
-			<p>Use the newly updated marketing materials module to promote your products and increse your sales.</p>
+		<h2>New feature available!</h2>
+		<p>Use the newly updated marketing materials module to promote your products and increse your sales.</p>
 	</div>
 </a>
-
+<img src="./img/footer.png" class="footer">
+<div id="overlay" onclick="closePopup()"></div>
+<div id="popup">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-<script type="text/javascript">
-	// Bring up the popup and overlay when download button is clicked
-	function openPopup() {
-	    var popup = document.getElementById("popup");
-	    var overlay = document.getElementById("overlay");
-    	popup.style.display = "block";
-   	 	overlay.style.display = "block";
-	}
-	// Closes the popup and overlay when users clicks outside of the popup
-	function closePopup() {
-		var popup = document.getElementById("popup");
-	    var overlay = document.getElementById("overlay");
-	    popup.style.display = "none";
-	    overlay.style.display = "none";
-	}
-
-	//Bring up the notification on any keypress
-	var notification = document.getElementById("notification");
-	window.addEventListener("keydown", function(e) {
-		notification.style.display ="block"});
-
-	//Deletes product from favourites
-	$( "#delete-favorite-product1" ).click(function() {
-	  	var remove = document.getElementById("product1");
-		remove.style.display = "none";
-	});
-
-	$( "#delete-favorite-product2" ).click(function() {
-	  	var remove = document.getElementById("product2");
-		remove.style.display = "none";
-	});
-
-	$( "#delete-favorite-product3" ).click(function() {
-	  	var remove = document.getElementById("product3");
-		remove.style.display = "none";
-	});
-
-	//Drag and drop sorting for products
-	$( function() {
-	    $( ".favorite-row" ).sortable();
-	    $( ".favorite-row" ).disableSelection();
-  	});
-
-  	// Moves  first product from last bought to favourites and replaces add button with remove button
-	$( "#add-product1" ).click(function() {
-	  	jQuery("#latest-product1").detach().appendTo('.favorite-row');
-		var addBtn = document.getElementById("add-product1");
-		var removeBtn = document.getElementById("delete-latest-product1");
-		addBtn.style.display = "none";
-	    removeBtn.style.display = "block";
-	});
-
-	// Moves first product from favourites to last bought and replaces remove button with add button
-	$( "#delete-latest-product1" ).click(function() {
-	  	jQuery("#latest-product1").detach().appendTo('.latest-purchases');
-		var addBtn = document.getElementById("add-product1");
-		var removeBtn = document.getElementById("delete-latest-product1");
-		addBtn.style.display = "block";
-	    removeBtn.style.display = "none";
-	});
-
-	 // Moves secondx` product from last bought to favourites and replaces add button with remove button
-	$( "#add-product2" ).click(function() {
-	  	jQuery("#latest-product2").detach().appendTo('.favorite-row');
-		var addBtn = document.getElementById("add-product2");
-		var removeBtn = document.getElementById("delete-latest-product2");
-		addBtn.style.display = "none";
-	    removeBtn.style.display = "block";
-	});
-
-	// Moves secondx` product from favourites to last bought and replaces remove button with add button
-	$( "#delete-latest-product2" ).click(function() {
-	  	jQuery("#latest-product2").detach().appendTo('.latest-purchases');
-		var addBtn = document.getElementById("add-product2");
-		var removeBtn = document.getElementById("delete-latest-product2");
-		addBtn.style.display = "block";
-	    removeBtn.style.display = "none";
-	});
-
-	 // Moves  third product from last bought to favourites and replaces add button with remove button
-	$( "#add-product3" ).click(function() {
-	  	jQuery("#latest-product3").detach().appendTo('.favorite-row');
-		var addBtn = document.getElementById("add-product3");
-		var removeBtn = document.getElementById("delete-latest-product3");
-		addBtn.style.display = "none";
-	    removeBtn.style.display = "block";
-	});
-
-	// Moves third product from favourites to last bought and replaces remove button with add button
-	$( "#delete-latest-product3" ).click(function() {
-	  	jQuery("#latest-product3").detach().appendTo('.latest-purchases');
-		var addBtn = document.getElementById("add-product3");
-		var removeBtn = document.getElementById("delete-latest-product3");
-		addBtn.style.display = "block";
-	    removeBtn.style.display = "none";
-	});
-	
-</script>
-    </body>
-    <img src="./img/footer.png" class="footer">
-    <div id="overlay" onclick="closePopup()"></div>
-	<div id="popup">
+<script src="scripts/script.js"></script>
+</body>
 </html>
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,106 @@
+$(document).ready( function () {
+    //Bring up the notification on any keypress
+    var notification = document.getElementById("notification");
+    window.addEventListener("keydown", function(e) {
+        var key = e.keyCode;
+            if (key === 69) {
+                notification.style.display ="block";
+            }
+    });
+
+    //Deletes product from favourites
+    $( "#delete-favorite-product1" ).click(function() {
+        var remove = document.getElementById("product1");
+        remove.style.display = "none";
+    });
+
+    $( "#delete-favorite-product2" ).click(function() {
+        var remove = document.getElementById("product2");
+        remove.style.display = "none";
+    });
+
+    $( "#delete-favorite-product3" ).click(function() {
+        var remove = document.getElementById("product3");
+        remove.style.display = "none";
+    });
+
+    //Drag and drop sorting for products
+    $( function() {
+        $( ".favorite-row" ).sortable();
+        $( ".favorite-row" ).disableSelection();
+    });
+
+    // Moves  first product from last bought to favourites and replaces add button with remove button
+    $( "#add-product1" ).click(function() {
+        jQuery("#latest-product1").detach().appendTo('.favorite-row');
+        var addBtn = document.getElementById("add-product1");
+        var removeBtn = document.getElementById("delete-latest-product1");
+        addBtn.style.display = "none";
+        removeBtn.style.display = "block";
+    });
+
+    // Moves first product from favourites to last bought and replaces remove button with add button
+    $( "#delete-latest-product1" ).click(function() {
+        jQuery("#latest-product1").detach().appendTo('.latest-purchases');
+        var addBtn = document.getElementById("add-product1");
+        var removeBtn = document.getElementById("delete-latest-product1");
+        addBtn.style.display = "block";
+        removeBtn.style.display = "none";
+    });
+
+     // Moves secondx` product from last bought to favourites and replaces add button with remove button
+    $( "#add-product2" ).click(function() {
+        jQuery("#latest-product2").detach().appendTo('.favorite-row');
+        var addBtn = document.getElementById("add-product2");
+        var removeBtn = document.getElementById("delete-latest-product2");
+        addBtn.style.display = "none";
+        removeBtn.style.display = "block";
+    });
+
+    // Moves secondx` product from favourites to last bought and replaces remove button with add button
+    $( "#delete-latest-product2" ).click(function() {
+        jQuery("#latest-product2").detach().appendTo('.latest-purchases');
+        var addBtn = document.getElementById("add-product2");
+        var removeBtn = document.getElementById("delete-latest-product2");
+        addBtn.style.display = "block";
+        removeBtn.style.display = "none";
+    });
+
+     // Moves  third product from last bought to favourites and replaces add button with remove button
+    $( "#add-product3" ).click(function() {
+        jQuery("#latest-product3").detach().appendTo('.favorite-row');
+        var addBtn = document.getElementById("add-product3");
+        var removeBtn = document.getElementById("delete-latest-product3");
+        addBtn.style.display = "none";
+        removeBtn.style.display = "block";
+    });
+
+    // Moves third product from favourites to last bought and replaces remove button with add button
+    $( "#delete-latest-product3" ).click(function() {
+        jQuery("#latest-product3").detach().appendTo('.latest-purchases');
+        var addBtn = document.getElementById("add-product3");
+        var removeBtn = document.getElementById("delete-latest-product3");
+        addBtn.style.display = "block";
+        removeBtn.style.display = "none";
+    });
+
+    $( function() {
+        var availableTags = [
+          "ALPHA 1",
+          "ALPHA 2",
+          "ALPHA 3",
+          "ALPHA1 L",
+          "CMBE",
+          "CMB-PM1",
+          "MAGNA1"
+        ];
+        $(".search-input").autocomplete({
+          source: availableTags
+        });
+    });  
+});
+
+
 var keyButton = document.querySelector("button.keyDropDown");
 var hiddenKeys = document.querySelector("div.hiddenKeys");
 var imgDrop = document.querySelector(".imgDrop");
@@ -20,3 +123,59 @@ function showMore2(){
 }
 keyButton.addEventListener('click', showMore);
 imgDrop.addEventListener("click", showMore2);
+
+// Bring up the popup and overlay when download button is clicked
+function openPopup() {
+    var popup = document.getElementById("popup");
+    var overlay = document.getElementById("overlay");
+    popup.style.display = "block";
+    overlay.style.display = "block";
+}
+// Closes the popup and overlay when users clicks outside of the popup
+function closePopup() {
+    var popup = document.getElementById("popup");
+    var overlay = document.getElementById("overlay");
+    popup.style.display = "none";
+    overlay.style.display = "none";
+}
+
+
+// Redirect user depending on the input
+function redirectSearch() {
+    var searchRequest = document.getElementById("search-product").value;
+    if(searchRequest == "ALPHA 1") {
+        window.location.href = "subpage.php";
+    } else if(searchRequest == "ALPHA 2") {
+        window.location.href = "subpage2.php";
+    } else if(searchRequest == "ALPHA 3") {
+        window.location.href = "subpage3.php";
+    };
+}
+
+
+// Adds products when using add product search bar
+function addProduct() {
+    var searchRequest = document.getElementById("add-product").value;
+    var product4 = document.getElementById("product4");
+        if(searchRequest == "ALPHA1 L") {
+            jQuery("#latest-product1").detach().appendTo('.favorite-row');
+            var addBtn = document.getElementById("add-product1");
+            var removeBtn = document.getElementById("delete-latest-product1");
+            addBtn.style.display = "none";
+            removeBtn.style.display = "block";
+        } else if(searchRequest == "CMBE") {
+            jQuery("#latest-product2").detach().appendTo('.favorite-row');
+            var addBtn = document.getElementById("add-product2");
+            var removeBtn = document.getElementById("delete-latest-product2");
+            addBtn.style.display = "none";
+            removeBtn.style.display = "block";
+        } else if(searchRequest == "CMB-PM1") {
+            jQuery("#latest-product3").detach().appendTo('.favorite-row');
+            var addBtn = document.getElementById("add-product3");
+            var removeBtn = document.getElementById("delete-latest-product3");
+            addBtn.style.display = "none";
+            removeBtn.style.display = "block";
+        } else if(searchRequest == "MAGNA1") {
+            product4.style.display = "block";
+        };
+}
