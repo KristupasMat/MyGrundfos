d6dca99bbe2cf82456ed28d490ee342c65a28a54
Tanngar <31502612+Tanngar@users.noreply.github.com>
2017-10-03 09:17:22 +0200

    Favorites functionality

--- index.php
+++ index.php
@@ -53,41 +53,55 @@
     </div>
       
 <h3>LATEST PURCHASES</h3>
-
-<section>
-<div class="product-box">
-    <img src="img/ALPHA1.png" width="auto" height="150px">
-<div class="updated-tag">
-  UPDATED
+<div class="latest-purchases">
+	<div class="product" id="latest-product1">
+		<div class="product-box">
+			<img src="img/ALPHA1.png" width="auto" height="160px">
+			<div class="updated-tag">
+				UPDATED
+			</div>
+			<button class="add-favorite-btn" id="add-product1">
+				<img src="./img/favorite-icon.svg">
+			</button>
+			<button class="remove-btn" id="delete-latest-product1">
+					
+			</button>
+		</div>
+		<p class="pump-name">ALPHA 1</p>
+		<button type="button" class="download btn" onclick="openPopup()">DOWNLOAD</button>
+	</div>
+	<div class="product" id="latest-product2">
+		<div class="product-box">
+			<img src="img/alpha2.jpg" width="auto" height="160px">
+			<div class="updated-tag">
+				UPDATED
+			</div>
+			<button class="add-favorite-btn" id="add-product2">
+				<img src="./img/favorite-icon.svg">
+			</button>
+			<button class="remove-btn" id="delete-latest-product2">
+					
+			</button>
+		</div>
+		<p class="pump-name">ALPHA 2</p>
+		<button type="button" class="download btn" onclick="openPopup()">DOWNLOAD</button>
+	</div>
+	<div class="product" id="latest-product3">
+		<div class="product-box">
+			<img src="img/alpha3.jpg" width="auto" height="160px">
+			<button class="add-favorite-btn" id="add-product3">
+				<img src="./img/favorite-icon.svg">	
+			</button>
+			<button class="remove-btn" id="delete-latest-product3">
+					
+			</button>
+			<p class="pump-name">ALPHA 3</p>
+			<button type="button" class="download btn" onclick="openPopup()">DOWNLOAD</button>
+		</div>
+	</div>
 </div>
-<i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
-</div>
-<p class="pump-name">ALPHA 1</p>
-<button type="button" class="download btn">DOWNLOAD</button>
-</section>
-
-<section class="section-latest">
-<div class="product-box">
-    <img src="img/alpha2.jpg" width="auto" height="150px">
-<div class="updated-tag">
-  UPDATED
-</div>
-<i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
-</div>
-<p class="pump-name">ALPHA 2</p>
-<button type="button" class="download btn">DOWNLOAD</button>
-</section>
-
-<section class="section-latest">
-<div class="product-box">
-<img src="img/alpha3.jpg" width="auto" height="150px">
-<i class="fa fa-plus-circle fa-3x" aria-hidden="true"></i>
-</div>
-<p class="pump-name">ALPHA 3</p>
-<button type="button" class="download btn">DOWNLOAD</button>
-</section>
 </aside>
-	<main id="wrapper2">
+<main id="wrapper2">
 	<h1 id="main-heading">MARKETING MATERIALS</h1>
 	<!-- Search form for marketing materials -->
 	<div class="search-form">
@@ -98,45 +112,54 @@
 		</form>
 	</div>
 <div class="favorite-row">
-  <section class="favorite">
-  <div class="favorite-box">
-      <img src="img/ALPHA1.png" width="auto" height="125px">
-  <div class="updated-tag">
-    UPDATED
-  </div>
-  </div>
-  <p class="pump-name">ALPHA 1</p>
-  <button type="button" class="download btn fav">DOWNLOAD</button>
-  </section>
-        
-          <section class="favorite">
-  <div class="favorite-box">
-      <img src="img/alpha2.jpg" width="auto" height="125px">
-  <div class="updated-tag">
-    UPDATED
-  </div>
-  </div>
-  <p class="pump-name">ALPHA 2</p>
-  <button type="button" class="download btn fav">DOWNLOAD</button>
-  </section>
-        
-          <section class="favorite">
-  <div class="favorite-box">
-      <img src="img/alpha3.jpg" width="auto" height="125px">
-  </div>
-  <p class="pump-name">ALPHA 3</p>
-  <button type="button" class="download btn fav">DOWNLOAD</button>
-  </section>
-        </div>
-  <!-- Search form for adding products to favourites -->
-  <div class="search-form">
-		<label><h2>Find a product you would like to add to your favourites:</h2></label>
-		<form>
-			 <input type="text" class="search-input" name="product">
-			 <input class="btn" type="submit" id="add-product" value="ADD PRODUCT TO FAVOURITES">
-		</form>
-        </div></main>
+	<div class="favorite" id="product1">
+		<div class="favorite-box">
+			<img src="img/ALPHA1.png" width="auto" height="160px">
+			<div class="updated-tag">
+				UPDATED
+			</div>
+			<button class="remove-btn" id="delete-favorite-product1">
+				<img src="./img/delete-icon.svg">	
+			</button>
+			<p class="pump-name">ALPHA 1</p>
+			<button type="button" class="download btn fav" onclick="openPopup()">DOWNLOAD</button>
+		</div>
+	</div>
+	<div class="favorite" id="product2">
+		<div class="favorite-box">
+			<img src="img/alpha2.jpg" width="auto" height="160px">
+			<div class="updated-tag">
+				UPDATED
+			</div>
+			<button class="remove-btn" id="delete-favorite-product2">
+				<img src="./img/delete-icon.svg">	
+			</button>
+			<p class="pump-name">ALPHA 2</p>
+			<button type="button" class="download btn fav" onclick="openPopup()">DOWNLOAD</button>
+		</div>
+	</div>
+  	<div class="favorite" id="product3">
+		<div class="favorite-box">
+			<img src="img/alpha3.jpg" width="auto" height="160px">
+			<button class="remove-btn" id="delete-favorite-product3">
+				<img src="./img/delete-icon.svg">	
+			</button>
+			<p class="pump-name">ALPHA 3</p>
+			<button type="button" class="download btn fav" onclick="openPopup()">DOWNLOAD</button>
+		</div>
+	</div>
 </div>
+
+<!-- Search form for adding products to favourites -->
+<div class="search-form">
+	<label><h2>Find a product you would like to add to your favourites:</h2></label>
+	<form>
+		 <input type="text" class="search-input" name="product">
+		 <input class="btn" type="submit" id="add-product" value="ADD PRODUCT TO FAVOURITES">
+	</form>
+    </div>
+</div>
+
 <!-- Section that will hold the notificaition message -->
 <a href="#">
 	<div id="notification">
@@ -144,20 +167,109 @@
 			<p>Use the newly updated marketing materials module to promote your products and increse your sales.</p>
 	</div>
 </a>
+
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script type="text/javascript">
+	// Bring up the popup and overlay when download button is clicked
+	function openPopup() {
+	    var popup = document.getElementById("popup");
+	    var overlay = document.getElementById("overlay");
+    	popup.style.display = "block";
+   	 	overlay.style.display = "block";
+	}
+	// Closes the popup and overlay when users clicks outside of the popup
+	function closePopup() {
+		var popup = document.getElementById("popup");
+	    var overlay = document.getElementById("overlay");
+	    popup.style.display = "none";
+	    overlay.style.display = "none";
+	}
+
 	//Bring up the notification on any keypress
 	var notification = document.getElementById("notification");
 	window.addEventListener("keydown", function(e) {
 		notification.style.display ="block"});
+
+	//Deletes product from favourites
+	$( "#delete-favorite-product1" ).click(function() {
+	  	var remove = document.getElementById("product1");
+		remove.style.display = "none";
+	});
+
+	$( "#delete-favorite-product2" ).click(function() {
+	  	var remove = document.getElementById("product2");
+		remove.style.display = "none";
+	});
+
+	$( "#delete-favorite-product3" ).click(function() {
+	  	var remove = document.getElementById("product3");
+		remove.style.display = "none";
+	});
+
+	//Drag and drop sorting for products
+	$( function() {
+	    $( ".favorite-row" ).sortable();
+	    $( ".favorite-row" ).disableSelection();
+  	});
+
+  	// Moves  first product from last bought to favourites and replaces add button with remove button
+	$( "#add-product1" ).click(function() {
+	  	jQuery("#latest-product1").detach().appendTo('.favorite-row');
+		var addBtn = document.getElementById("add-product1");
+		var removeBtn = document.getElementById("delete-latest-product1");
+		addBtn.style.display = "none";
+	    removeBtn.style.display = "block";
+	});
+
+	// Moves first product from favourites to last bought and replaces remove button with add button
+	$( "#delete-latest-product1" ).click(function() {
+	  	jQuery("#latest-product1").detach().appendTo('.latest-purchases');
+		var addBtn = document.getElementById("add-product1");
+		var removeBtn = document.getElementById("delete-latest-product1");
+		addBtn.style.display = "block";
+	    removeBtn.style.display = "none";
+	});
+
+	 // Moves secondx` product from last bought to favourites and replaces add button with remove button
+	$( "#add-product2" ).click(function() {
+	  	jQuery("#latest-product2").detach().appendTo('.favorite-row');
+		var addBtn = document.getElementById("add-product2");
+		var removeBtn = document.getElementById("delete-latest-product2");
+		addBtn.style.display = "none";
+	    removeBtn.style.display = "block";
+	});
+
+	// Moves secondx` product from favourites to last bought and replaces remove button with add button
+	$( "#delete-latest-product2" ).click(function() {
+	  	jQuery("#latest-product2").detach().appendTo('.latest-purchases');
+		var addBtn = document.getElementById("add-product2");
+		var removeBtn = document.getElementById("delete-latest-product2");
+		addBtn.style.display = "block";
+	    removeBtn.style.display = "none";
+	});
+
+	 // Moves  third product from last bought to favourites and replaces add button with remove button
+	$( "#add-product3" ).click(function() {
+	  	jQuery("#latest-product3").detach().appendTo('.favorite-row');
+		var addBtn = document.getElementById("add-product3");
+		var removeBtn = document.getElementById("delete-latest-product3");
+		addBtn.style.display = "none";
+	    removeBtn.style.display = "block";
+	});
+
+	// Moves third product from favourites to last bought and replaces remove button with add button
+	$( "#delete-latest-product3" ).click(function() {
+	  	jQuery("#latest-product3").detach().appendTo('.latest-purchases');
+		var addBtn = document.getElementById("add-product3");
+		var removeBtn = document.getElementById("delete-latest-product3");
+		addBtn.style.display = "block";
+	    removeBtn.style.display = "none";
+	});
+	
 </script>
-
-
-                
-            
-
-            
-        
-
     </body>
     <img src="./img/footer.png" class="footer">
+    <div id="overlay" onclick="closePopup()"></div>
+	<div id="popup">
 </html>
